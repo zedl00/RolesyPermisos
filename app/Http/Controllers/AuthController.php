8e577d7fb1e602eb474fcb84a19204551919ab83
zedl00 <nestorvaldez@hotmail.com>
2023-08-23 19:41:20 -0400

    Autenticacion Token API Rest Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
+
+class AuthController extends Controller
+{
+    public function funIngresar(Request $request){
+
+        $credenciales = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Validar correo y password
+        if(!Auth::attempt($credenciales)){
+            return response()->json(["mensaje" => "No Autenticado"], 401);
+        }
+
+        // Generar Token
+        $user = Auth::user();
+        $tokenResult = $user->createToken("Token Personal");
+        $token = $tokenResult->plainTextToken;  
+
+        // Respuesta
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => "Bearer",
+            "usuario" => $user
+        ]);
+    }
+
+    public function funRegistro(Request $request){
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        // Respuesta
+
+        return response()->json(["mensaje" => "El Usuario ha sido registrado"], 201);
+
+    }
+
+    public function funPerfil() 
+    {
+        $usuario = Auth::user();
+        return response()->json($usuario, 200);
+    }
+
+    public function funSalir(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(["mensaje" => "Logout"], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1/auth')->group(function(){
+    Route::post("/login", [AuthController::class, "funIngresar"]);
+    Route::post("/register", [AuthController::class, "funRegistro"]);
+
+    Route::middleware('auth:sanctum')->group(function(){
+    
+        Route::get("/perfil",  [AuthController::class, "funPerfil"]);
+        Route::post("/logout", [AuthController::class, "funSalir"]);
+    });
+});                  
+
