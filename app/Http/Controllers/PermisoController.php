fc92c1241409c8191b43a5a0f2c00b5916ac4a16
zedl00 <nestorvaldez@hotmail.com>
2023-08-23 22:20:14 -0400

    Migraciones y CRUD Usuarios

--- app/Http/Controllers/PermisoController.php
+++ app/Http/Controllers/PermisoController.php
@@ -2,47 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permiso;
 use Illuminate\Http\Request;
 
 class PermisoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $permisos = Permiso::get();
+
+        return response()->json($permisos);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:permisos"
+        ]);
+
+        $permiso = new Permiso();
+        $permiso->nombre = $request->nombre;
+        $permiso->detalle = $request->detalle;
+        $permiso->save();
+
+        return response()->json(["mensaje" => "El Permiso ha sido registrado"]);
+        
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $permiso = Permiso::findOrFail($id);
+
+        return response()->json($permiso, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:permisos,nombre,$id"
+        ]);
+
+        $permiso = Permiso::findOrFail($id);
+        $permiso->nombre = $request->nombre;
+        $permiso->detalle = $request->detalle;
+        $permiso->update();
+
+        return response()->json(["mensaje" => "El Permiso ha sido Actualizado", 201]);
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $permiso = Permiso::findOrFail($id);
+
+        $permiso->delete();
+
+        return response()->json(["mensaje" => "El Permiso ha sido Eliminado"]);
     }
 }
--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,47 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        $roles = Roles::get();
+
+        return response()->json($roles);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:roles"
+        ]);
+
+        $role = new Role();
+        $role->nombre = $request->nombre;
+        $role->detalle = $request->detalle;
+        $role->save();
+
+        return response()->json(["mensaje" => "El Rol ha sido registrado"]);
+        
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $rol = Role::findOrFail($id);
+
+        return response()->json($rol, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:roles,nombre,$id"
+        ]);
+
+        $role = Role::findOrFail($id);
+        $role->nombre = $request->nombre;
+        $role->detalle = $request->detalle;
+        $role->update();
+
+        return response()->json(["mensaje" => "El Rol ha sido Actualizado", 201]);
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $role = Role::findOrFail($id);
+
+        $role->delete();
+
+        return response()->json(["mensaje" => "El Rol ha sido Eliminado"]);
     }
 }
